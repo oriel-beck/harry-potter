575ab92f7bec40b576e12f859f9f9c8ea08e1a2d
oriel-beck <orielbeck@gmail.com>
2022-08-14 12:47:48 +0300

    ready for heroku

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get("x-forwarded-proto") !== "https") {
+    return res.redirect("https://" + req.get("host") + req.url);
+  }
+  next();
+}
+
+const app = express();
+
+app.use(requireHTTPS);
+
+app.use(express.static("./dist/harry-potter/"));
+
+app.get("/*", function (req, res) {
+  res.sendFile("index.html", { root: "dist/harry-potter/" });
+});
+
+app.listen(8080);
